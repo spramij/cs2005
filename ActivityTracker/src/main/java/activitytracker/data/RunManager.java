721e9dfc0a6e40995ca29cd2b04951a7de938dcf
SimantoR <md.simanto_rahman@yahoo.com>
2018-11-27 20:57:52 -0330

    Added: - RunManager.java to handle Run data and all kind of statistical outputs

---
+++ ActivityTracker/src/main/java/activitytracker/data/RunManager.java
@@ -0,0 +1,66 @@
+package activitytracker.data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+
+import activitytracker.ClassData;
+
+public class RunManager {
+
+    private List<ClassData> runDatas;
+
+    /**
+     * Create a new empty instance of Run Manager
+     */
+    public RunManager() {
+        runDatas = new ArrayList<ClassData>();
+    }
+
+    /**
+     * Create a new instance of Run Manager
+     * @param runDatas Array of Data to populate this Run Manager
+     */
+    public RunManager(ClassData[] runDatas) {
+        this.runDatas = new ArrayList<ClassData>();
+
+        for (ClassData data : runDatas) {
+            this.runDatas.add(data);
+        }
+    }
+
+    @JsonGetter("run_data")
+    /**
+     * @return All the Run data available
+     */
+    public ClassData[] getRunDatas() {
+        return runDatas.toArray(new ClassData[runDatas.size()]);
+    }
+
+    /**
+     * @param from Lower limit to the date-based filter
+     * @param to   Higher limti to the date-base filter
+     * @return All the run data within given date frame
+     */
+    public ClassData[] getRunDatas(Date from, Date to) {
+        return null;
+    }
+
+    @JsonSetter(value = "run_data", nulls = Nulls.SKIP)
+    /**
+     * Assigns run data to the instance
+     * 
+     * @param dataSet Array of Data to store in RunManager instance
+     */
+    public void setRunDatas(ClassData[] dataSet) {
+        runDatas.clear();
+
+        for (ClassData data : dataSet) {
+            runDatas.add(data);
+        }
+    }
+}
